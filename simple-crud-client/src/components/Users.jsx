ccd3bde398c01155b204027a4615950ac1734107
a1shuvo <a1shuvosaha@gmail.com>
2025-05-13 05:41:49 +0600

    Insert user data into mongodb

--- simple-crud-client/src/components/Users.jsx
+++ simple-crud-client/src/components/Users.jsx
@@ -5,6 +5,17 @@ const Users = () => {
     const email = e.target.email.value;
     const newUser = { name, email };
     console.log(newUser);
+
+    // Create user in the DB
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log("Data after creating user in the DB", data));
   };
   return (
     <div>
